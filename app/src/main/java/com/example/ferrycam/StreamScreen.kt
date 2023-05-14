6bf61a0864a802b551d841ded93415e72f5db3ac
Josh Edwards <josh@terasoft.com.au>
2023-05-14 17:18:55 +1000

    Changed stream screen to show both streams on portrait and vertical

--- app/src/main/java/com/example/ferrycam/StreamScreen.kt
+++ app/src/main/java/com/example/ferrycam/StreamScreen.kt
@@ -1,25 +1,24 @@
 package com.example.ferrycam
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalConfiguration
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
 @Composable
 fun StreamScreen() {
     var selectedButton by remember { mutableStateOf(0) }
+    /*
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -29,7 +28,48 @@ fun StreamScreen() {
             1 -> StreamView(stringResource(R.string.settlement_queue_url))
             else -> StreamView(stringResource(R.string.settlement_ferry_url))
         }
+    }*/
+
+    val configuration = LocalConfiguration.current
+    if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+                StreamView(stringResource(R.string.settlement_ferry_url))
+            }
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+                StreamView(stringResource(R.string.settlement_queue_url))
+            }
+        }
+    } else {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            when (selectedButton) {
+                1 -> StreamView(stringResource(R.string.settlement_queue_url))
+                else -> StreamView(stringResource(R.string.settlement_ferry_url))
+            }
+            Box(
+                modifier = Modifier
+                    .width(250.dp)
+                    .height(100.dp)
+                    .align(Alignment.BottomEnd)
+            ) {
+                when (selectedButton) {
+                    0 -> StreamView(stringResource(R.string.settlement_queue_url))
+                    else -> StreamView(stringResource(R.string.settlement_ferry_url))
+                }
+            }
+        }
     }
+
+    /*
     Row(
     verticalAlignment = Alignment.Bottom,
     horizontalArrangement = Arrangement.Center,
@@ -46,7 +86,7 @@ fun StreamScreen() {
         Button(onClick = { selectedButton = 1 }) {
             Text(stringResource(R.string.settlement_queue_button).uppercase())
         }
-    }
+    }*/
 }
 
 @Composable
